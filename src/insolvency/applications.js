c4aa5cd9f588cea02d2a11eb99b8c55f108dbe42
Dawid <dawidpol1@gmail.com>
2018-03-06 09:26:13 +0800

    Add application status

---
+++ src/insolvency/applications.js
@@ -0,0 +1,6 @@
+export const ApplicationStatus = {}
+
+ApplicationStatus[ApplicationStatus['Pending'] = 'pending'] = 'Pending'
+ApplicationStatus[ApplicationStatus['Reviewing'] = 'reviewing'] = 'Reviewing'
+ApplicationStatus[ApplicationStatus['Accepted'] = 'accepted'] = 'Accepted'
+ApplicationStatus[ApplicationStatus['Rejected'] = 'rejected'] = 'Rejected'
